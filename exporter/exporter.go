47c752394d55fbf8eb225d8bd4e0d375f6d3e40e
Håkon Åmdal <hakon@aamdal.com>
2018-09-10 12:53:07 +0200

    fixup! Basic implementation

--- exporter/exporter.go
+++ exporter/exporter.go
@@ -26,7 +26,7 @@ type Exporter struct {
 	githubToken string
 }
 
-type ReadmeResponse struct {
+type readmeResponse struct {
 	Content string `json:"content"`
 }
 
@@ -38,43 +38,13 @@ func NewExporter(repos []string, githubToken string) Exporter {
 	return e
 }
 
-// Describe sends the super-set of all possible descriptors of metrics
-// collected by this Collector to the provided channel and returns once
-// the last descriptor has been sent. The sent descriptors fulfill the
-// consistency and uniqueness requirements described in the Desc
-// documentation.
-//
-// It is valid if one and the same Collector sends duplicate
-// descriptors. Those duplicates are simply ignored. However, two
-// different Collectors must not send duplicate descriptors.
-//
-// Sending no descriptor at all marks the Collector as “unchecked”,
-// i.e. no checks will be performed at registration time, and the
-// Collector may yield any Metric it sees fit in its Collect method.
-//
-// This method idempotently sends the same descriptors throughout the
-// lifetime of the Collector.
-//
-// If a Collector encounters an error while executing this method, it
-// must send an invalid descriptor (created with NewInvalidDesc) to
-// signal the error to the registry.
+// Describe forwards the Readme metric description
 func (exporter *Exporter) Describe(ch chan<- *prometheus.Desc) {
 	// We need to send a description. What do we send?
 	ch <- readme
 }
 
-// Collect is called by the Prometheus registry when collecting
-// metrics. The implementation sends each collected metric via the
-// provided channel and returns once the last metric has been sent. The
-// descriptor of each sent metric is one of those returned by Describe
-// (unless the Collector is unchecked, see above). Returned metrics that
-// share the same descriptor must differ in their variable label
-// values.
-//
-// This method may be called concurrently and must therefore be
-// implemented in a concurrency safe way. Blocking occurs at the expense
-// of total performance of rendering all registered metrics. Ideally,
-// Collector implementations support concurrent readers.
+// Collect loops the available repositories and returns a readme metric gauge per repo
 func (exporter *Exporter) Collect(ch chan<- prometheus.Metric) {
 	for _, repo := range exporter.repos {
 		url := fmt.Sprintf("https://%s@api.github.com/repos/%s/readme", exporter.githubToken, repo)
@@ -106,7 +76,7 @@ func getRepoReadmeLength(url string) (int, error) {
 	if err != nil {
 		return 0, errors.New("Could not read response")
 	}
-	r := ReadmeResponse{}
+	r := readmeResponse{}
 	jsonErr := json.Unmarshal(body, &r)
 	if jsonErr != nil {
 		return 0, errors.New("Unable to read JSON response")
